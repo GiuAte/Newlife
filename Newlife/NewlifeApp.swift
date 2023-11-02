d1b9636d198a1f094dca062223dd40107b60a283
GiuAte <giulioaterno@gmail.com>
2023-11-02 10:19:18 +0100

    Initial Commit

---
+++ Newlife/NewlifeApp.swift
@@ -0,0 +1,17 @@
+//
+//  NewlifeApp.swift
+//  Newlife
+//
+//  Created by Giulio Aterno on 02/11/23.
+//
+
+import SwiftUI
+
+@main
+struct NewlifeApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
