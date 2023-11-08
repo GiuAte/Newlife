68c6446ed7c00a9c76d946d2b90b156d78cdb962
GiuAte <giulioaterno@gmail.com>
2023-11-08 14:17:08 +0100

    Added notifications

--- Newlife/NewlifeApp.swift
+++ Newlife/NewlifeApp.swift
@@ -19,3 +19,4 @@ struct NewlifeApp: App {
         }
     }
 }
+
--- Views/ContentView.swift
+++ Views/ContentView.swift
@@ -6,14 +6,70 @@
 //
 
 import SwiftUI
+import UserNotifications
 
 struct ContentView: View {
     @State private var selectedTab: Tab? = .home
     
+    init() {
+        checkForPermission()
+        dispatchNotification()
+    }
+    
     var body: some View {
         TabBarView()
     }
+    
+    func checkForPermission() {
+        let notificationCenter = UNUserNotificationCenter.current()
+        
+        notificationCenter.getNotificationSettings { settings in
+            switch settings.authorizationStatus {
+            case .authorized:
+                self.dispatchNotification()
+            case .denied:
+                print("L'utente ha negato l'autorizzazione alle notifiche.")
+            case .notDetermined:
+                notificationCenter.requestAuthorization(options: [.alert, .sound]) { didAllow, error in
+                    if didAllow {
+                        self.dispatchNotification()
+                    }
+                }
+            default:
+                return
+            }
+        }
+    }
+    
+    
+    func dispatchNotification() {
+        let identifier = "my-afternoon-notification"
+        let title = "Prepara i rifiuti!"
+        let body = "Scopri cosa smaltire oggi"
+        let hour = 17
+        let minute = 30
+        let isDaily = true
+        
+        let notificationCenter = UNUserNotificationCenter.current()
+        
+        let content = UNMutableNotificationContent()
+        content.title = title
+        content.body = body
+        content .sound = .default
+        
+        let calendar = Calendar.current
+        var dateComponents = DateComponents(calendar: calendar, timeZone: TimeZone.current)
+        dateComponents.hour = hour
+        dateComponents.minute = minute
+        
+        let trigger = UNCalendarNotificationTrigger(dateMatching: dateComponents, repeats: isDaily)
+        let request = UNNotificationRequest(identifier: identifier, content: content, trigger: trigger)
+        
+        notificationCenter.removePendingNotificationRequests(withIdentifiers: [identifier])
+        notificationCenter.add(request)
+    }
 }
+
 
 #Preview {
     ContentView()
